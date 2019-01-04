991ac49ccb1ebca332024fe9b126f316b00c1c9c
Ingo Heimbach <IJ_H@gmx.de>
2019-01-04 08:08:31 +0100

    Change the code style to match `black`

--- vimtips/__main__.py
+++ vimtips/__main__.py
@@ -7,5 +7,5 @@ def main():
     gui.main()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- vimtips/config.py
+++ vimtips/config.py
@@ -2,15 +2,12 @@ import os
 from configparser import ConfigParser
 from typing import Any, Dict, Optional, TextIO, Union  # noqa: F401  # pylint: disable=unused-import
 
-DEFAULT_CONFIG_FILENAME = os.path.expanduser('~/.vimtips.conf')
+DEFAULT_CONFIG_FILENAME = os.path.expanduser("~/.vimtips.conf")
 
 
 class Config:
     _default_config = {
-        'gui': {
-            'command': 'vimtips-gui',
-            'last_start_by_daemon': 0.0
-        }
+        "gui": {"command": "vimtips-gui", "last_start_by_daemon": 0.0}
     }  # type: Dict[str, Dict[str, Any]]
 
     def __init__(self, config_filename: str = DEFAULT_CONFIG_FILENAME) -> None:
@@ -27,20 +24,20 @@ class Config:
     def write_config(self, config_filename: Optional[str] = None) -> None:
         if config_filename is None:
             config_filename = self._config_filename
-        with open(config_filename, 'w') as f:
+        with open(config_filename, "w") as f:
             self._config.write(f)
 
     @property
     def gui_command(self) -> str:
-        return self._config['gui']['command']
+        return self._config["gui"]["command"]
 
     @property
     def gui_last_start_by_daemon(self) -> float:
-        return float(self._config['gui']['last_start_by_daemon'])
+        return float(self._config["gui"]["last_start_by_daemon"])
 
     @gui_last_start_by_daemon.setter
     def gui_last_start_by_daemon(self, value: float) -> None:
-        self._config['gui']['last_start_by_daemon'] = str(value)
+        self._config["gui"]["last_start_by_daemon"] = str(value)
         self.write_config()
 
 
