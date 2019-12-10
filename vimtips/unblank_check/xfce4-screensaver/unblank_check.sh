88c7493805ab160477c05efbbb0382bd7fd8b68e
Ingo Heimbach <IJ_H@gmx.de>
2019-12-10 17:56:54 +0100

    Add support for the GNOME and Xfce screensavers

---
+++ vimtips/unblank_check/xfce4-screensaver/unblank_check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DESKTOP_ENVIRONMENT="xfce"  # can be `xfce` or `gnome`
+
+while read -r line; do
+    case "${line}" in
+        *"boolean false"*)
+            echo "unblank"
+            ;;
+    esac
+done < <(dbus-monitor --session "type='signal',interface='org.${DESKTOP_ENVIRONMENT}.ScreenSaver'")
