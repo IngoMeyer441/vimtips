9aef668f900339776502282a7d90b3009eb256db
Ingo Heimbach <IJ_H@gmx.de>
2019-12-11 15:55:58 +0100

    Fix a possible segmentation fault in the X11 unblank checker

--- vimtips/unblank_check/x11/unblank_check.c
+++ vimtips/unblank_check/x11/unblank_check.c
@@ -10,18 +10,22 @@
 
 
 static const char *check_display_state(void) {
-    Display *dpy = XOpenDisplay(NULL);
+    Display *display;
     int dummy;
     BOOL onoff;
     CARD16 state;
 
-    if (!DPMSQueryExtension(dpy, &dummy, &dummy)) {
+    display = XOpenDisplay(NULL);
+    if (display == NULL) {
         return NULL;
     }
-    if (!DPMSCapable(dpy)) {
+    if (!DPMSQueryExtension(display, &dummy, &dummy)) {
         return NULL;
     }
-    DPMSInfo(dpy, &state, &onoff);
+    if (!DPMSCapable(display)) {
+        return NULL;
+    }
+    DPMSInfo(display, &state, &onoff);
     if (!onoff) {
         return NULL;
     }
@@ -44,18 +48,19 @@ int main(void) {
     const char *current_display_state, *previous_display_state;
 
     current_display_state = previous_display_state = check_display_state();
-    do {
-        if (current_display_state != previous_display_state) {
+    while (1) {
+        if (current_display_state != NULL && current_display_state != previous_display_state) {
             printf("%s\n", current_display_state);
             fflush(stdout);
             previous_display_state = current_display_state;
         }
         sleep(QUERY_INTERVAL);
-    } while ((current_display_state = check_display_state()) != NULL);
+        current_display_state = check_display_state();
+    }
 
     return 1;
 }
 
 #else
-#error This program can only be used on macos.
+#error This program can only be used on linux.
 #endif
